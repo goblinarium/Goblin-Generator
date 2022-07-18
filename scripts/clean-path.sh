d3241735fe46b640c6380f7eed6624630c6e54a4
Max Rafferty <git@maxrafferty.com>
2022-07-18 19:53:45 -0400

    deploy

--- scripts/clean-path.sh
+++ scripts/clean-path.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sed -i -e 's/href="/href="\/s4/g' docs/index.html
-sed -i -e 's/src="/src="\/s4/g' docs/index.html
+sed -i -e 's/href="/href="\/Goblin-Generator/g' docs/index.html
+sed -i -e 's/src="/src="\/Goblin-Generator/g' docs/index.html
 sed -i -e 's/<\/head>/<script>window.S4_CONSTANTS={isProd: true};<\/script><\/head>/g' docs/index.html
